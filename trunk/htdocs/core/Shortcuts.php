07350bdd1b1cbb6e6fc5be719a08a1fbe79c8e95
v2ex.livid <v2ex.livid@b521dfde-9c19-0410-8b12-fd397a4aaa36>
2007-03-05 12:53:50 +0000

    Use CDN as much as more.

--- trunk/htdocs/core/Shortcuts.php
+++ trunk/htdocs/core/Shortcuts.php
@@ -30,6 +30,7 @@ function _v_btn_f($label, $form) {
 	$form_md5 = md5($form);
 	$container = 'btn_' . strval(rand(1111, 9999));
 	echo('<script type="text/javascript">');
+	echo('var i = new Image(15,15); i.src="' . CDN_UI . 'img/loading.gif";');
 	echo('var f_' . $form_md5 . "_do = function() {\n");
 	echo('var c = getObj("' . $container . '"); c.innerHTML = "<img src=' . CDN_UI . 'img/loading.gif align=absmiddle /> <span class=tip_i>正在发送请求</span>";');
 	echo('var o = getObj("' . $form . '"); return o.submit();');
