706cf93bee3df966a7105a8a5b189ae804524f95
v2ex.livid <v2ex.livid@b521dfde-9c19-0410-8b12-fd397a4aaa36>
2007-08-02 20:20:43 +0000

    [I18N] Supported languages defined.

---
+++ res/supported_languages.php
@@ -0,0 +1,14 @@
+<?php
+$_languages = array(
+	'zh_cn' => '简体中文',
+	'zh_tw' => '正体中文',
+	'en_us' => 'US English',
+	'fr_fr' => 'Français',
+	'de_de' => 'Deutsch',
+	'es_es' => 'Español',
+	'ja_jp' => '日本語',
+	'ko_kr' => '한국어',
+	'pt_pt' => 'Português',
+	'pl_pl' => 'Polski'
+);
+?>
