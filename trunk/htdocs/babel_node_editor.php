d98192b50352f38f6957f3c606beb0a298cab44e
v2ex.livid <v2ex.livid@b521dfde-9c19-0410-8b12-fd397a4aaa36>
2007-08-18 22:12:58 +0000

    To Tokyo.

---
+++ trunk/htdocs/babel_node_editor.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" lang="zh-CN">
+<head>
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<meta http-equiv="cache-control" content="no-cache" />
+<meta name="keywords" content="V2EX, Babel, Livid, PHP" />
+<title>Project Babel Node Editor</title>
+<link href="/favicon.ico" rel="shortcut icon" />
+<style type="text/css">
+body {
+padding: 0px;
+margin: 0px;
+background-color: #222;
+color: #FFF;
+font-family: "Lucida Grande", Verdana, Tahoma, hei;
+}
+
+h1 {
+padding: 10px;
+margin: 0px;
+background-color: #445;
+border-bottom: 1px solid #222;
+font-size: 18px;
+font-weight: normal;
+color: #FFF;
+}
+
+h2 {
+font-size: 15px;
+font-family: "Lucide Grande", "Arial";
+font-weight: normal;
+color: #CCC;
+padding: 10px 0px 5px 15px;
+margin: 0px;
+}
+
+div.box_bright {
+color: #333;
+background-color: #F0F0F0;
+border: 2px solid #EEE;
+-webkit-border-radius: 7px;
+-moz-border-radius: 7px;
+font-size: 12px;
+padding: 10px;
+margin: 10px;
+}
+
+td.section_left {
+background-color: #333;
+}
+
+td.section_right {
+background-color: #222;
+}
+
+div.copyright {
+font-size: 10px;
+padding: 10px;
+text-align: right;
+line-height: 16px;
+}
+</style>
+</head>
+<body>
+<h1>Project Babel Node Editor</h1>
+<table cellpadding="0" cellspacing="0" border="0" width="100%">
+<tr>
+<td width="220" class="section_left">
+<h2>Locating</h2>
+<div class="box_bright"><input type="text" /> <input type="button" value="Get" /></div>
+<h2>Node Tree</h2>
+</td>
+<td width="auto" class="section_right">Hello
+</td>
+</table>
+<div class="copyright">Project Babel Node Editor -- An utility for nodes' customization and localization<br />&copy; 2007 Xin Liu</div>
+</body>
+</html>
