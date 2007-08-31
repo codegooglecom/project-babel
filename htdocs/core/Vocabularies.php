d3ad74349f79f9f3f2d2ab33570ea1d6ef434359
v2ex.livid <v2ex.livid@b521dfde-9c19-0410-8b12-fd397a4aaa36>
2007-08-31 19:38:46 +0000

    [UI] Minor tweaking.

--- htdocs/core/Vocabularies.php
+++ htdocs/core/Vocabularies.php
@@ -22,7 +22,7 @@ class Vocabulary {
 	const site_name = 'V2EX';
 	const site_title = 'V2EX | Project Babel';
 	const site_title_mobile = 'V2EX Mobile';
-	const site_copyright = 'Project Babel | v0.6-dev Fibonacci Farm | Copyright © 2007 Xin Liu (a.k.a. <a href="http://www.livid.cn/" target="_blank">Livid</a>)';
+	const site_copyright = 'Project Babel v0.6-dev Fibonacci Farm - Copyright © 2007 Xin <a href="http://nexus.v2ex.com/livid" target="_blank">Livid</a> Liu';
 	
 	const site_banner = "<a href=\"/\" target=\"_self\" class=\"var\"><img src=\"/img/top_logo_carbon.gif\" border=\"0\" alt=\"V2EX\" onclick=\"location.href='/';\" style=\"cursor: hand;\" align=\"absmiddle\" onmouseover=\"focusGo();\" /></a>";
 	
