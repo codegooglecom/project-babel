4e456ae3068583448a326fabbdf3a7fd4690178f
v2ex.livid <v2ex.livid@b521dfde-9c19-0410-8b12-fd397a4aaa36>
2007-10-06 19:23:16 +0000

    To be continued.

---
+++ trunk/scripts/fetch_dependencies.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+cp -R /www/babel/htdocs/img/icons/silk ../htdocs/img/icons/
+cp -R /www/babel/htdocs/img/icons/mini ../htdocs/img/icons/
+cp -R /www/babel/htdocs/img/icons/flag ../htdocs/img/icons/
+cp -R /www/babel/htdocs/img/icons/tango ../htdocs/img/icons/
+cp -R /www/babel/libs/zf/1.0.2 ../libs/zf/
