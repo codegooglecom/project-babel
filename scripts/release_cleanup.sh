7794392aa07576208584407146274463e7fe36d0
v2ex.livid <v2ex.livid@b521dfde-9c19-0410-8b12-fd397a4aaa36>
2007-10-06 19:03:33 +0000

    [Scripts] Cleanup script for release.

---
+++ scripts/release_cleanup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cp /dev/null ../res/google_adsense_top.php
+cp /dev/null ../res/google_analytics.php
+cp /dev/null ../res/alimama_top.php
+rm ../res/hot*.html
+cp /dev/null ../res/hot.html
+echo "<h1 class=silver>Community Guidelines</h1>" > ../res/community_guidelines.html
